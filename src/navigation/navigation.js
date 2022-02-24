1c1bdca2d9e9dab596227a58e40a68cdc290e24b
Abdulmalik Ibrahim <ibrahimmalik095@gmail.com>
2022-02-24 02:20:26 +0100

    chore: Add Login Screen

--- src/navigation/navigation.js
+++ src/navigation/navigation.js
@@ -3,7 +3,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import Onboarding from '../screens/Onboarding/Onboarding';
 import WelcomeScreen from '../screens/WelcomeScreen/Welcome';
-import SignupScreen from '../screens/Signup/SignupScreen';
+import LoginScreen from '../screens/Login/LoginScreen';
 
 const Stack = createStackNavigator();
 
@@ -13,7 +13,7 @@ function Navigator() {
       <Stack.Navigator headerMode="none">
         <Stack.Screen name="Onboarding" component={Onboarding} />
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
-        <Stack.Screen name="Signup" component={SignupScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
