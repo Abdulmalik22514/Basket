a4017af2440596ae78576809307a2e66d6d1c4b0
Abdulmalik Ibrahim <ibrahimmalik095@gmail.com>
2022-02-24 02:19:41 +0100

    chore: Add icons and svg

--- src/common/icons.js
+++ src/common/icons.js
@@ -1,7 +1,11 @@
 const trolley = require('../../assets/icons/cart.png');
 const family = require('../../assets/icons/family.png');
+const facebook = require('../../assets/icons/facebook.png');
+const google = require('../../assets/icons/google.png');
 
 export default {
   trolley,
   family,
+  facebook,
+  google,
 };
