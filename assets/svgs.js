e11c3f23d8839faf3eae652d5d655fe5c366ce49
Abdulmalik Ibrahim <ibrahimmalik095@gmail.com>
2022-02-25 07:49:58 +0100

    chore: Add image icons and svg icons

--- assets/svgs.js
+++ assets/svgs.js
@@ -102,3 +102,54 @@ export const Tick = props => {
     </Svg>
   );
 };
+
+export const SearchIcon = props => {
+  return (
+    <Svg width="18" height="18" viewBox="0 0 18 18" fill="none" {...props}>
+      <Path
+        d="M8.25 14.25C11.5637 14.25 14.25 11.5637 14.25 8.25C14.25 4.93629 11.5637 2.25 8.25 2.25C4.93629 2.25 2.25 4.93629 2.25 8.25C2.25 11.5637 4.93629 14.25 8.25 14.25Z"
+        stroke="#112D42"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <Path
+        d="M15.75 15.75L12.4875 12.4875"
+        stroke="#112D42"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </Svg>
+  );
+};
+
+export const MenuIcon = () => {
+  return (
+    <Svg
+      width="43"
+      height="20"
+      viewBox="0 0 43 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg">
+      <Path
+        d="M2 2H41"
+        stroke="#FF8137"
+        stroke-width="3"
+        stroke-linecap="round"
+      />
+      <Path
+        d="M2 10H32"
+        stroke="#FF8137"
+        stroke-width="3"
+        stroke-linecap="round"
+      />
+      <Path
+        d="M2 18H24"
+        stroke="#FF8137"
+        stroke-width="3"
+        stroke-linecap="round"
+      />
+    </Svg>
+  );
+};
--- src/common/icons.js
+++ src/common/icons.js
@@ -2,10 +2,36 @@ const trolley = require('../../assets/icons/cart.png');
 const family = require('../../assets/icons/family.png');
 const facebook = require('../../assets/icons/facebook.png');
 const google = require('../../assets/icons/google.png');
+const home = require('../../assets/icons/home.png');
+const saved = require('../../assets/icons/saved.png');
+const account = require('../../assets/icons/account.png');
+const activity = require('../../assets/icons/activity.png');
+const bell = require('../../assets/icons/bell.png');
+const gear = require('../../assets/icons/gear.png');
+const logout = require('../../assets/icons/logout.png');
+const payment = require('../../assets/icons/payment.png');
+const register = require('../../assets/icons/register.png');
+const star = require('../../assets/icons/star.png');
+const tracker = require('../../assets/icons/tracker.png');
+const daniel = require('../../assets/icons/daniel.png');
+const cart = require('../../assets/icons/cart2.png');
 
 export default {
   trolley,
   family,
   facebook,
   google,
+  home,
+  saved,
+  account,
+  cart,
+  activity,
+  bell,
+  gear,
+  logout,
+  payment,
+  register,
+  star,
+  tracker,
+  daniel,
 };
